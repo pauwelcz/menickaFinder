cf27b0330c1e09f6818a26ff9c5a562560a66d69
pauwelcz <pavel.sedlar1993@gmail.com>
2021-06-25 08:54:45 +0200

    First commit

---
+++ 00.dailyMenuZMenicka.js
@@ -0,0 +1,48 @@
+'use strict';
+const parser = require('./lib/parsersWithPredefinedModules.js');
+const printer = require('./lib/printModules.js');
+/*
+	COMMANDER
+*/
+const program = require('commander')
+    program
+        .option('-r, --restaurants <string>', 'File of objects with restaurants', './data/restaurants.json')
+        .option('-o, --output <string>', 'Parameter for saving file, if parameter is not set, output is printed to console.', '')
+        .option('-n, --name <string>', 'Parameter for name of file (in combination with parameter output), if parameter is not set, name of file is in format "DD-MM-YYYY.txt".', '')
+        .parse(process.argv);
+
+const restaurants = require(`${program.restaurants}`);
+
+/**
+    FILE PREPARINGS
+ */
+const fileDirectory = program.output;
+const fileName = program.name;
+const homeDir = __dirname;
+
+(async function main () {
+    const restaurants = require(`${program.restaurants}`);
+    /**
+     * Ziskani spravneho datumu
+     */
+    const dateToday = new Date();
+    const dateTodayParsed = {
+        day: dateToday.getDate(),
+        month: dateToday.getMonth() + 1,
+        year: dateToday.getFullYear(),
+    }
+
+    /**
+     * Projeti restauraci v souboru
+     */
+    const menuObjects = [];
+    for (const object of restaurants) {
+        menuObjects.push(await parser.menickaParser(object, dateTodayParsed));
+    }
+
+    /**
+     * Vytisknutí buď do souboru nebo na obrazovku
+     */
+     await printer.printMenu(menuObjects, dateTodayParsed, fileDirectory, fileName, homeDir);
+     process.exit();
+})();
---
+++ 01.dailyMenu.js
@@ -0,0 +1,54 @@
+'use strict';
+const parser = require('./lib/parsersWithPredefinedModules.js');
+const printModules = require('./lib/printModules.js');
+/*
+	COMMANDER
+*/
+const program = require('commander')
+    program
+        .option('-r, --restaurants <string>', 'File of objects with restaurants', './data/restaurants.json')
+        .option('-o, --output <string>', 'Parameter for saving file, if parameter is not set, output is printed to console.', '')
+        .option('-n, --name <string>', 'Parameter for name of file (in combination with parameter output), if parameter is not set, name of file is in format "DD-MM-YYYY.txt".', '')
+        .parse(process.argv);
+
+/**
+    FILE PREPARINGS
+ */
+const fileDirectory = program.output;
+const fileName = program.name;
+const homeDir = __dirname;
+
+(async function main() {
+    const restaurants = require(`${program.restaurants}`);
+    /**
+     * Ziskani spravneho datumu + dne v týdnu (v případě některých webů se snáze parsuje)
+     */
+    const dateToday = new Date();
+    const dateTodayParsed = {
+        day: dateToday.getDate(),
+        month: dateToday.getMonth() + 1,
+        year: dateToday.getFullYear(),
+        dayInWeek: dateToday.getDay(),
+    }
+
+    const menuObjects = [];
+    /**
+     * Restaurace VERONA
+     */
+    menuObjects.push(await parser.menickaParser(restaurants[2], dateTodayParsed));
+    /**
+     * Restaurace Suzie
+     */
+    menuObjects.push(await parser.suziesCheerioParser(restaurants[1], dateTodayParsed));
+
+    /**
+     * Restaurace u Čápa
+     */
+    menuObjects.push(await parser.uCapaCheriioParser(restaurants[0], dateTodayParsed));
+
+    /**
+     * Vytisknutí buď do souboru nebo na obrazovku
+     */
+    await printModules.printMenu(menuObjects, dateTodayParsed, fileDirectory, fileName, homeDir);
+    process.exit();
+})();
